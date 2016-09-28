1ef0d2fedf28a76c3ec47f461f5a72352c83a9d2
flysamc <flysamc@gmail.com>
2016-09-28 23:12:48 +0530

    codes filling in linearRegCostFunction , learning curves
    problem still in learning curve have to figure it.

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -87,6 +87,7 @@ lambda = 0;
 
 %  Plot fit over the data
 plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+
 xlabel('Change in water level (x)');
 ylabel('Water flowing out of the dam (y)');
 hold on;
