fd2f184f0b7339c656c94e62d66a9bb91aa8c554
flysamc <flysamc@gmail.com>
2016-09-30 18:27:25 +0530

    completed problemsheets
    q3 is simple but i was thinking it tough, get the hang after
    working for whole days

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -1,5 +1,9 @@
 function [error_train, error_val] = ...
-    learningCurve(X, y, Xval, yval, lambda)
+    learningCurve( X, y, ...
+                   Xval, yval, ...
+                  lambda)
+
+
 %LEARNINGCURVE Generates the train and cross validation set errors needed 
 %to plot a learning curve
 %   [error_train, error_val] = ...
@@ -16,6 +20,8 @@ function [error_train, error_val] = ...
 
 % Number of training examples
 m = size(X, 1);
+n = size(Xval,1);
+
 
 % You need to return these values correctly
 error_train = zeros(m, 1);
@@ -52,11 +58,21 @@ error_val   = zeros(m, 1);
 %
 
 % ---------------------- Sample Solution ----------------------
-%theta = [1 ; 1];
+
+%error_train = linearRegCostFunction([ones(m, 1) X], y, theta, 0);
+%error_val   = linearRegCostFunction([ones(m, 1) Xval], yval, theta, 0);
+
+
 for i=1:m
-   error_train(i) = linearRegCostFuntion([ones(i, 1) X(1:i, :)], y(1:i), theta, 0);
-   error_val(i)   = linearRegCostFuntion([ones(i, 1) Xval(1:i, :)], yval(1:i), theta, 0);
+    theta = trainLinearReg(X(1:i,:), y(1:i),lambda);
+    error_train(i,1) = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+    error_val(i,1) = linearRegCostFunction(Xval, yval, theta, 0);
+     
 end
+
+
+   
+
 
 
 
--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -38,7 +38,13 @@ error_val = zeros(length(lambda_vec), 1);
 %       end
 %
 %
-
+for i = 1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    theta = trainLinearReg(X,y,lambda);
+    lambda =0;
+    error_train(i) = linearRegCostFunction(X, y, theta, lambda);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, lambda);
+end
 
 
 
