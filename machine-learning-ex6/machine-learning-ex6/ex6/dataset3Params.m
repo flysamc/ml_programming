f312905a51b3a7c9b1585603cf0af9d370c5554f
flysamc <flysamc@gmail.com>
2016-10-04 23:16:14 +0530

    fgh
    fgh

--- machine-learning-ex6/machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/machine-learning-ex6/ex6/dataset3Params.m
@@ -22,7 +22,27 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+ Cv = [0.01,0.03,0.1,0.3,1,3,10,30];
+ sv = [0.01,0.03,0.1,0.3,1,3,10,30];
+ temp = inf;
+ for i=1:8
+    C1 = Cv(1,i);
+    for j=1:8
+        s1 = sv(1,j);
+        model= svmTrain(X, y, C1, @(x1, x2) gaussianKernel(x1, x2, s1));
+        pred = svmPredict(model, Xval);
+         err = mean(double(pred ~= yval));
 
+         if(err<temp && err>0)
+            temp = err;
+            C = C1;
+            sigma = s1;
+         end
+    end
+end
+
+        
+    
 
 
 
--- machine-learning-ex6/machine-learning-ex6/ex6/ex6.m
+++ machine-learning-ex6/machine-learning-ex6/ex6/ex6.m
@@ -70,7 +70,7 @@ fprintf(['Gaussian Kernel between x1 = [1; 2; 1], x2 = [0; 4; -1], sigma = 0.5 :
          '\n\t%f\n(this value should be about 0.324652)\n'], sim);
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+%pause;
 
 %% =============== Part 4: Visualizing Dataset 2 ================
 %  The following code will load the next dataset into your environment and 
@@ -126,7 +126,7 @@ load('ex6data3.mat');
 plotData(X, y);
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+%pause;
 
 %% ========== Part 7: Training SVM with RBF Kernel (Dataset 3) ==========
 
